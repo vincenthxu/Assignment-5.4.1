518968c25d020eea7311f473761b27252c0288b2
Vincent Xu <vincent.xu1@outlook.com>
2025-08-07 09:42:00 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_5._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDigits(1234);
+        }
+
+        static void PrintDigits(int num)
+        {
+            if (num / 10 > 0)
+            {
+                PrintDigits(num / 10);
+            }
+            Console.Write($"{num % 10} ");
+        }
+    }
+}
